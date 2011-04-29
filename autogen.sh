a525a5d3be28a6d13f3962e178f67eb6ab027894
Andrea Mazzoleni <amadvance@users.sourceforge.net>
2011-04-29 17:48:05 +0200

    Adds -i to autoreconf in autoget.sh.

--- autogen.sh
+++ autogen.sh
@@ -3,7 +3,7 @@
 echo "Generating build information using autoreconf"
 
 # All is done by autoreconf
-autoreconf
+autoreconf -i
 
 # Run configure for this platform
 echo "Now you are ready to run ./configure"
