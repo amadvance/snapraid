9195f60a3613587219b68879f9ac2c42d5349de2
Andrea Mazzoleni <amadvance@users.sourceforge.net>
2013-03-05 19:09:09 +0100

    Fixes copyright year.

--- import.h
+++ import.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2014 Andrea Mazzoleni
+ * Copyright (C) 2013 Andrea Mazzoleni
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
