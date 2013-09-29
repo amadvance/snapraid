02e11256de563109ab4816bbd8a96fe11d049636
Andrea Mazzoleni <amadvance@gmail.com>
2013-09-29 08:20:45 +0200

    Adds a faster distribution with -f without checking.

--- makedist.sh
+++ makedist.sh
@@ -7,7 +7,13 @@ if ! ./configure.windows-x86; then
 	exit 1
 fi
 
-if ! make check distwindows-x86 distclean; then
+if ! test "x$1" = "x-f"; then
+	if ! make check; then
+		exit 1
+	fi
+fi
+
+if ! make distwindows-x86 distclean; then
 	exit 1
 fi
 
