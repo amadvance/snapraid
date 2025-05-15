09897fcdb35c99b0147415f0118f21d4d90b22ad
Andrea Mazzoleni <amadvance@gmail.com>
2025-05-15 21:51:10 +0200

    Extend autover to work also without git tags

--- autover.sh
+++ autover.sh
@@ -3,17 +3,22 @@
 
 if [ -d .git ]; then
     # Get version from git tags, removing the 'v' prefix
-    VERSION=`git describe --match 'v*' 2>/dev/null | sed 's/^v//'`
+    VERSION=$(git describe --match 'v*' 2>/dev/null | sed 's/^v//')
 fi
 
 if [ -f .version ]; then
     # Get version from the .version file
-    VERSION=`cat .version`
+    VERSION=$(cat .version)
+fi
+
+if [ -z "$VERSION" ] && [ -d .git ]; then
+    # Fall back to short commit hash
+    VERSION=0-$(git rev-parse --short HEAD 2>/dev/null)
 fi
 
 if [ -z $VERSION ]; then
-    VERSION="none"
+    # No version, but still use a number
+    VERSION="0"
 fi
 
 printf '%s' "$VERSION"
-
