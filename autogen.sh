01347e48b6715952a7615d1a3a7ddd0b330fb76b
Andrea Mazzoleni <amadvance@gmail.com>
2026-02-13 12:39:21 +0100

    Remove backup files created by autoreconf

--- autogen.sh
+++ autogen.sh
@@ -8,3 +8,5 @@ autoreconf -f -i
 # Run configure for this platform
 echo "Now you are ready to run ./configure"
 
+# Remove backup files created by autoreconf
+rm -f config.guess~ config.h.in~ config.sub~ configure~ install-sh~
