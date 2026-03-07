5ad1a586b07bcfdfa850f8ab2337795234c1cca1
Andrea Mazzoleni <amadvance@gmail.com>
2026-03-07 17:26:11 +0100

    More compliant Slackware package

---
+++ pkg/doinst.sh
@@ -0,0 +1,14 @@
+config() {
+  NEW="$1"
+  OLD="$(dirname $NEW)/$(basename $NEW .new)"
+  # If there's no config file by that name, mv it over:
+  if [ ! -r $OLD ]; then
+    mv $NEW $OLD
+  elif [ "$(cat $OLD | md5sum)" = "$(cat $NEW | md5sum)" ]; then
+    # toss the redundant copy
+    rm $NEW
+  fi
+  # Otherwise, we leave the .new copy for the admin to consider...
+}
+
+config etc/snapraid.conf.new
