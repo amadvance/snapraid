2198f8736a843272539a72560b33ec1ca24d613b
Andrea Mazzoleni <amadvance@gmail.com>
2026-04-29 14:51:40 +0200

    More coverage support

--- makecov.sh
+++ makecov.sh
@@ -3,6 +3,9 @@
 # Run the Coverage test
 #
 
+# Ask root permissions at the start
+sudo echo We are root
+
 make distclean
 
 if ! ./configure --enable-coverage --enable-sde; then
@@ -17,6 +20,9 @@ if ! make check; then
 	exit 1
 fi
 
+# Root test for Btrfs
+sudo make btrfscheck
+
 # Run commands on a real array to test smartctl
 sudo ./snapraid probe
 sudo ./snapraid up
@@ -24,7 +30,11 @@ sudo ./snapraid smart
 sudo ./snapraid probe
 sudo ./snapraid devices
 sudo ./snapraid down
+sudo ./snapraid test-downifup
 
 if ! make lcov_capture lcov_html; then
 	exit 1
 fi
+
+# Remove root generated files
+sudo make clean
